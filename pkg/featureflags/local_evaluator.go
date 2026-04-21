e78e9a8905b63960d18e37cc4950a598c7d9966a
Akim Sadaoui <akim.sadaoui@datadoghq.com>
2026-04-21 17:33:20 +0200

    fix: disable --x-parallelparsing CLI flag
    The flag triggers race conditions in concurrent query workers
    causing non-deterministic violation counts.

--- pkg/featureflags/local_evaluator.go
+++ pkg/featureflags/local_evaluator.go
@@ -11,7 +11,7 @@ func NewLocalEvaluatorWithOverrides(overrides map[string]bool) *LocalEvaluator {
 		IacDisableKicsRule:               false,
 		IacEnableKicsPlatform:            true,
 		IacEnableKicsHelmResolver:        true,
-		IaCEnableKicsParallelFileParsing: true,
+		IaCEnableKicsParallelFileParsing: false,
 	}
 
 	for k, v := range overrides {
