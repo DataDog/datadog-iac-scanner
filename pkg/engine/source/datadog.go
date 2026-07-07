d5ab63dfc929234d4ec1643183f51fe35fb03a14
gaspard.barbier <gaspard.barbier@datadoghq.com>
2026-07-07 16:40:11 +0200

    Change metadata arguments' shape

--- pkg/engine/source/datadog.go
+++ pkg/engine/source/datadog.go
@@ -253,7 +253,7 @@ func ConvertRule(rule *datadog.Rule) model.QueryMetadata {
 		out.Metadata["cwe"] = ""
 	}
 	if len(rule.Arguments) > 0 {
-		out.Metadata["arguments"] = rule.Arguments
+		out.Metadata["arguments"] = ruleArgumentsToMeta(rule.Arguments)
 	}
 	if rule.Aggregation != nil {
 		out.Metadata["aggregation"] = *rule.Aggregation
@@ -301,6 +301,17 @@ func frameworksToMeta(fs []datadog.Framework) []any {
 			"framework_version": f.Version,
 			"requirement":       f.Requirement,
 			"control":           f.Control,
+		}
+	}
+	return result
+}
+
+func ruleArgumentsToMeta(args []datadog.RuleArgument) []any {
+	result := make([]any, len(args))
+	for i, arg := range args {
+		result[i] = map[string]any{
+			"name":        arg.Name,
+			"description": arg.Description,
 		}
 	}
 	return result
